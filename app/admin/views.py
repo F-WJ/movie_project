c7f5de4b4409413ea7430555c3cc8b1327dcfe8c
FWJ <364580936@qq.com>
2017-09-14 01:02:40 +0800

    搭建后台视图

--- app/admin/views.py
+++ app/admin/views.py
@@ -2,11 +2,42 @@
 __author__ = 'FWJ'
 __date__ = 2017 / 8 / 9
 from . import admin
+from flask import render_template, redirect, url_for
 
 
-# 调用蓝图
+# 系统管理界面
 @admin.route("/")
 def index():
-    return "<h1 style='color:red'>this is admin<h1>"
+    return render_template("admin/index.html")
+
+
+# 登录
+@admin.route("/login/")
+def login():
+    return render_template("admin/login.html")
+
+
+# 退出登录
+@admin.route("/logout/")
+def logout():
+    return redirect(url_for("admin.login"))
+
+
+# 修改密码界面
+@admin.route("/pwd/")
+def pwd():
+    return render_template("admin/pwd.html")
+
+
+# 编辑标签
+@admin.route("/tag/add/")
+def tag_add():
+    return render_template("admin/tag_add.html")
+
+
+# 标签列表
+@admin.route("/tag/list/")
+def tag_list():
+    return render_template("admin/tag_list.html")
 
 
